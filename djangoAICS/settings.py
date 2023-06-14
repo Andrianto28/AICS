ca2669f891ff1bed66622ad778b3533483a49e8d
Andrianto28 <muhamadandrianto091@gmail.com>
2023-06-14 10:53:51 +0700

    add locationMovement to be global folder

--- djangoAICS/settings.py
+++ djangoAICS/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'inputtingAutomation1'
+    'inputtingAutomation1',
+    'locationMovement3'
 ]
 
 MIDDLEWARE = [
